e2835b15bef15ad45a7892d200bf2a565bee9397
Makkarty <ivan_ivanov@mail.com>
2026-06-23 21:04:20 +0300

    Fix task 4: update var name

--- homework/Nick_Kablitsky/Lesson_4/task1.py
+++ homework/Nick_Kablitsky/Lesson_4/task1.py
@@ -1,58 +1,40 @@
-# просто кортеж
-# tuple = (varTuple_1, varTuple_2, varTuple_3, varTuple_4, varTuple_5)
-#
-# список
-# list = [varList_1, varList_2, varList_3, varList_4, varList_5]
-#
-# просто словарь
-# dict = {
-#   'keyDict1': varDict1,
-#   'keyDict2': varDict2,
-#   'keyDict3': varDict3,
-#   'keyDict4': varDict4,
-#   'keyDict5': varDict5
-# }
-#
-# просто множество
-# set = {varSet_1, varSet_2, varSet_3, varSet_4, varSet_5}
+# Объявляем переменные
+var_tuple_1, var_tuple_2, var_tuple_3, var_tuple_4, var_tuple_5 = 1, 2, 3, 4, 5
+var_list_1, var_list_2, var_list_3, var_list_4, var_list_5 = 'one', 'two', 'three', 'four', 'five'
+var_dict_1, var_dict_2, var_dict_3, var_dict_4, var_dict_5 = 'I', 'like', 'Yamaha', 'and', 'Honda'
+var_set_1, var_set_2, var_set_3, var_set_4, var_set_5 = 1, 2, 3, 4, 5
 
-# в словарь пихаем каждый элемент
-
-varTuple_1, varTuple_2, varTuple_3, varTuple_4, varTuple_5 = 1, 2, 3, 4, 5
-varList_1, varList_2, varList_3, varList_4, varList_5 = 'one', 'two', 'three', 'four', 'five'
-varDict_1, varDict_2, varDict_3, varDict_4, varDict_5 = 'I', 'like', 'Yamaha', 'and', 'Honda'
-varSet_1, varSet_2, varSet_3, varSet_4, varSet_5 = 1, 2, 3, 4, 5
-
+# Создаем словарь
 my_dict = {
-    'tuple': (varTuple_1, varTuple_2, varTuple_3, varTuple_4, varTuple_5),
-    'list': [varList_1, varList_2, varList_3, varList_4, varList_5],
+    'tuple': (var_tuple_1, var_tuple_2, var_tuple_3, var_tuple_4, var_tuple_5),
+    'list': [var_list_1, var_list_2, var_list_3, var_list_4, var_list_5],
     'dict': {
-        'keyDict1': varDict_1,
-        'keyDict2': varDict_2,
-        'keyDict3': varDict_3,
-        'keyDict4': varDict_4,
-        'keyDict5': varDict_5
+        'key_dict1': var_dict_1,
+        'key_dict2': var_dict_2,
+        'key_dict3': var_dict_3,
+        'key_dict4': var_dict_4,
+        'key_dict5': var_dict_5
     },
-    'set': {varSet_1, varSet_2, varSet_3, varSet_4, varSet_5}
+    'set': {var_set_1, var_set_2, var_set_3, var_set_4, var_set_5}
 }
 
 # Для того, что хранится под ключом 'tuple': выведите на экран последний элемент
 print(my_dict['tuple'][-1])
 
 # Для того, что хранится под ключом 'list': добавьте в конец списка еще один элемент, удалите второй элемент списка.
-varList_6 = 'six'
-my_dict['list'].append(varList_6)
+var_list_6 = 'six'
+my_dict['list'].append(var_list_6)
 my_dict['list'].pop(1)
 
 # Для того, что хранится
 # под ключом 'dict': добавьте элемент с ключом ('i am a tuple',), любым значением удалите какой-нибудь элемент.
 my_dict['dict'][('i am a tuple',)] = '1'
-del my_dict['dict']['keyDict4']
+del my_dict['dict']['key_dict_4']
 
 # Для того, что хранится под ключом 'set': добавьте новый элемент в множеств, удалите элемент из множества.
-varSet_6 = 6
-my_dict['set'].add(varSet_6)
-my_dict['set'].remove(varSet_6)
+var_set_6 = 6
+my_dict['set'].add(var_set_6)
+my_dict['set'].remove(var_set_6)
 
 # Выод на экран словря
 print(my_dict)
