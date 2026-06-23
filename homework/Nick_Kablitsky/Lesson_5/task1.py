8e1fef974f9cbe4795d5d32c879ff91bb34f5d09
Makkarty <ivan_ivanov@mail.com>
2026-06-24 02:32:06 +0300

    Complete HW_5

--- homework/Nick_Kablitsky/Lesson_5/task1.py
+++ homework/Nick_Kablitsky/Lesson_5/task1.py
@@ -1,4 +1,4 @@
 # С помощью распаковки создайте из этого списка переменные, содержащие соответствующие данные:
 # name, last_name, city, phone, country
 person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
-name, last_name, city, phone, country  = person
+name, last_name, city, phone, country = person
