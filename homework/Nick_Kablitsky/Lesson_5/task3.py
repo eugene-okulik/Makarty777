b0bc6321f261bfcec927af883f0cd780c8e9f94a
Makkarty <ivan_ivanov@mail.com>
2026-06-24 02:26:21 +0300

    Complete HW_5

---
+++ homework/Nick_Kablitsky/Lesson_5/task3.py
@@ -0,0 +1,6 @@
+# Даны такие списки:
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+# Распечатайте текст, который будет использовать данные из этих списков. Текст в итоге должен выглядеть так:
+print('Students', ', '.join(students), 'study these subjects: ', ', '.join(subjects))
