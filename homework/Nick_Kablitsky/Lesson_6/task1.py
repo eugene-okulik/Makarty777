6391a9e2b202a2e2b8a32f2bff21ae1c80c84be7
Makkarty <ivan_ivanov@mail.com>
2026-06-28 15:18:16 +0300

    Nikita_Kablitsky_homework_6

--- homework/Nick_Kablitsky/Lesson_6/task1.py
+++ homework/Nick_Kablitsky/Lesson_6/task1.py
@@ -6,7 +6,8 @@
 # но уже преобразованного.
 
 # Непосредственно текст
-text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. ' \
+'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
 
 # Инициализация словаря и дробление текста
 words = text.split()
