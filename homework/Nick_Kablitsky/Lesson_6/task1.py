2c03777d8e73adf2db648644292166fdfa1f3f07
Makkarty <ivan_ivanov@mail.com>
2026-06-28 15:38:06 +0300

    Nikita_Kablitsky_homework_6

--- homework/Nick_Kablitsky/Lesson_6/task1.py
+++ homework/Nick_Kablitsky/Lesson_6/task1.py
@@ -7,7 +7,7 @@
 
 # Непосредственно текст
 text = (
-    'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
+    'Etiam tincidunt neque erat, quis molestie enim imperdiet vel.'
     'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
 )
 
