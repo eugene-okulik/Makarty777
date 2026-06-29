9f11326f4d00bc5b4e92456a86ff44aac8518b19
Makkarty <ivan_ivanov@mail.com>
2026-06-29 19:36:15 +0300

    Nikita_Kablitsky_homework_7

---
+++ homework/Nick_Kablitsky/Lesson_8/task1.py
@@ -0,0 +1,29 @@
+# Напишите программу. Есть две переменные, salary и bonus. Salary - int, bonus - bool.
+# Спросите у пользователя salary. А bonus пусть назначается рандомом.
+# Если bonus - true, то к salary должен быть добавлен рандомный бонус.
+# Примеры результатов:
+# 10000, True - '$10255'
+# waaaaa25000, False - '$25000'
+# 600, True - '$3785'
+
+import random
+while True:
+    salary, bonus = input('Введите целое число: '), bool(random.randint(0, 1))
+    if salary.isdigit() == True:
+        if bonus == True:
+            print(f"{salary}, {bonus} - '${salary + random.randint(1, 10000)}' ")
+        if bonus == False:
+            print(f"{salary}, {bonus} - '${salary}' ")
+        break
+    else:
+        print('Число неверно! Но не переживай сейчас мы соберем его ;)')
+        sclad_id_digit = []
+        for kasdaya in salary:
+            if kasdaya.isdigit() == True:
+                sclad_id_digit.append(kasdaya)
+        salary = int(''.join(sclad_id_digit))
+        if bonus == True:
+            print(f"{salary}, {bonus} - '${salary + random.randint(1, 10000)}' ")
+        if bonus == False:
+            print(f"{salary}, {bonus} - '${salary}' ")
+        break
