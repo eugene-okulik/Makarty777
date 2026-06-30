7d91197dcfe6d2c7ed16c7c9d47c3bccfb3c3493
Makkarty <ivan_ivanov@mail.com>
2026-06-30 17:23:23 +0300

    Nikita_Kablitsky_homework_8

--- homework/Nick_Kablitsky/Lesson_8/task3.py
+++ homework/Nick_Kablitsky/Lesson_8/task3.py
@@ -1,8 +1,7 @@
-# Настройте виртуальное окружение на своем компе. Ниже ссылка на видео о виртуальных окружениях. 
+# Настройте виртуальное окружение на своем компе. Ниже ссылка на видео о виртуальных окружениях.
 # Там есть куча деталей. Если будут возникать проблемы, что-то будет не получаться - пишите.
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
 
 driver = webdriver.Chrome()
 driver.get('https://www.google.com/')
@@ -10,3 +9,5 @@ search_input = driver.find_element(By.NAME, 'q')
 search_input.send_keys('cat')
 search_input.submit()
 driver.quit()
+
+# ЧЕК ЛИЧКУ В ТГ ПЖПЖЖПЖПЖПЖ
