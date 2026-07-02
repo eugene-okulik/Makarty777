f35389fcd3a535b8c778cf95a858c4b780cd02e4
Makkarty <ivan_ivanov@mail.com>
2026-07-03 01:55:45 +0300

    Nikita_Kablitsky_homework_9

---
+++ homework/Nick_Kablitsky/Lesson_9/task1.py
@@ -0,0 +1,13 @@
+# Дана такая дата: "Jan 15, 2023 - 12:05:33"
+# Преобразуйте эту дату в питоновский формат, после этого:
+
+import datetime, locale
+
+# Принудительно инглиш взял с нейронки
+locale.setlocale(locale.LC_TIME, 'English_United States.1252')
+
+data = 'Jan 15, 2023 - 12:05:33'
+# 1. Распечатайте полное название месяца из этой даты
+print(datetime.datetime.strptime(data, '%b %d, %Y - %H:%M:%S').strftime('%B'))
+# 2. Распечатайте дату в таком формате: "15.01.2023, 12:05"
+print(datetime.datetime.strptime(data, '%b %d, %Y - %H:%M:%S').strftime('%d.%m.%Y, %H:%M'))
---
+++ homework/Nick_Kablitsky/Lesson_9/task2.py
@@ -0,0 +1,20 @@
+# map | filter
+
+# Есть такой список:
+temperatures = [
+20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 
+22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23
+]
+
+# С помощью функции map или filter создайте из этого списка новый список с жаркими днями.
+new_temperatures = filter(lambda: temperatures*1.15 ,temperatures)
+print(new_temperatures)
+
+# Будем считать жарким всё, что выше 28.
+INNA_Hot = list(filter(lambda x: x > 28, temperatures))
+print(f'Горячие хиты краснодара 2007: {INNA_Hot}')
+
+# Распечатайте из нового списка самую высокую температуру самую низкую и среднюю.
+var_max, var_min = max(temperatures), min(temperatures)
+print(f'Самая высокая температура: {var_max}; Самая низкая температура: {var_min}')
+print(f'А средняя температура составляет: {(sum(temperatures)) / len(temperatures)}')
