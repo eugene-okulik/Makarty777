04a9da28d90587688b3cf1323fb6d435f252b70a
Makkarty <ivan_ivanov@mail.com>
2026-07-07 06:33:38 +0300

    Nikita_Kablitsky_homework_12

--- homework/Nick_Kablitsky/task1.py
+++ homework/Nick_Kablitsky/task1.py
@@ -1,61 +1,102 @@
 # Создать классы цветов: 
 # общий класс для всех цветов  +
 # классы для нескольких видов  +
-# Создать экземпляры (объекты) цветов разных видов. Собрать букет (букет - еще один класс) 
-# с определением его стоимости. В букете цветы пусть хранятся в списке. Это будет список объектов.
+# Создать экземпляры (объекты) цветов разных видов. +
+# Собрать букет (букет - еще один класс) с определением его стоимости. +
+# В букете цветы пусть хранятся в списке. Это будет список объектов.+
 # Для букета создать метод, который определяет время его увядания по среднему времени жизни всех цветов в букете.
 # Позволить сортировку цветов в букете на основе различных параметров 
 # (свежесть/цвет/длина стебля/стоимость)(это тоже методы)
 # Реализовать поиск цветов в букете по каким-нибудь параметрам (например, по среднему времени жизни) (и это тоже метод).
 
-import random
 
+class Bouquet():
+    def __init__(self):
+        self.flowers = []
 
-сlass Bouquet():
+    def add_flower(self, flower):
+        self.flowers.append(flower)
+
+    def total_price_Bouquet(self):    
+        return sum(flower.price for flower in self.flowers)
+
+    def wilting_time(self):
+        return sum(flower.chastota_yxashivania for flower in self.flowers) / len(self.flowers)
+
+    def find_arg_lifetime(self, min_days):
+        return [flower for flower in self.flowers if flower.chastota_yxashivania >= min_days]
+
+    def sort_by_price(self):
+        self.flowers.sort(key=lambda flower: flower.price)
+
+    def search_flower_parameters(
+            self, find_arg_lifetime = None, fresh = None, color = None, leng_cabels = None, price = None):
+        resault = []
+        for flower in self.flowers:
+            if find_arg_lifetime is not None and flower.chastota_yxashivania < find_arg_lifetime:
+                continue
+            if fresh is not None and flower.fresh != fresh:
+                continue
+            if color is not None and flower.color != color:
+                continue
+            if price is not None and flower.price < price: 
+                continue
+            resault.append(flower)
+        return resault
+
 
 class Flowers():
-    def __init__(self, zapah, lepestki, chastota_yxashivania, price):
+    def __init__(self, zapah, lepestki, chastota_yxashivania, price, fresh, color, leng_cabels):
         self.zapah = zapah 
         self.lepestki = lepestki
         self.chastota_yxashivania = chastota_yxashivania
         self.price = price
+        self.fresh = fresh
+        self.color = color
+        self.leng_cabels = leng_cabels
+
+    def __str__(self):
+        return f'Цвет {self.color}, время жизни: {self.chastota_yxashivania} дней, цена: {self.price}'
+    
+    def __repr__(self):
+        return self.__str__()
 
 
 class HomeFlowers(Flowers):
-    def __init__(self, zapah, lepestki, chastota_yxashivania, price, zdanie):
-        self.zapah = zapah 
-        self.lepestki = lepestki
-        self.chastota_yxashivania = chastota_yxashivania
-        self.price = price
+    def __init__(self, zapah, lepestki, chastota_yxashivania, price, fresh, color, leng_cabels, zdanie):
+        super().__init__(zapah, lepestki, chastota_yxashivania, price, fresh, color, leng_cabels)
         self.zdanie = zdanie
 
 class PresentFlowers(Flowers):
-    def __init__(self, zapah, lepestki, chastota_yxashivania, price, podarok):
-        self.zapah = zapah 
-        self.lepestki = lepestki
-        self.chastota_yxashivania = chastota_yxashivania
-        self.price = price
+    def __init__(self, zapah, lepestki, chastota_yxashivania, price, fresh, color, leng_cabels, podarok):
+        super().__init__(zapah, lepestki, chastota_yxashivania, price, fresh, color, leng_cabels)
         self.podarok = podarok
 
-Byket = []
 
-flower1 = HomeFlowers(zapah=False, lepestki=True, chastota_yxashivania=7, price=500, zdanie=True)
-flower2 = HomeFlowers(zapah=True, lepestki=False, chastota_yxashivania=365, price=0, zdanie=True)
-flower3 = PresentFlowers(zapah=True, lepestki=True, chastota_yxashivania=2, price=730, podarok=True)
-flower4 = PresentFlowers(zapah=True, lepestki=True, chastota_yxashivania=1, price=8000, podarok=True)
+flower1 = HomeFlowers(
+    zapah = False, lepestki = True, chastota_yxashivania = 7, price = 500, 
+    zdanie = True, fresh = True, color = 'Red', leng_cabels = 0.2
+    )
+flower2 = HomeFlowers(
+    zapah = True, lepestki = False, chastota_yxashivania = 365, price = 0, zdanie = True,
+    fresh = True, color = 'Blue', leng_cabels = 0.5
+    )
+flower3 = PresentFlowers(
+    zapah = True, lepestki = True, chastota_yxashivania = 2, price = 730, podarok = True,
+    fresh = True, color = 'White', leng_cabels = 0.3
+    )
+flower4 = PresentFlowers(
+    zapah = True, lepestki = True, chastota_yxashivania = 1, price = 8000, podarok = True,
+    fresh = False, color = 'Red', leng_cabels = 0.5
+    )
 
-Byket.append(flower1)
-Byket.append(flower2)
-Byket.append(flower3)
-Byket.append(flower4)
+my_bouquet = Bouquet()
+my_bouquet.add_flower(flower1)
+my_bouquet.add_flower(flower2)
+my_bouquet.add_flower(flower3)
+my_bouquet.add_flower(flower4)
 
-print(f"В букете {len(Byket)} цветов")
-total_price = random.randint(120, 1000000)
-wilting_time = ((flower1.chastota_yxashivania + flower2.chastota_yxashivania + 
-                 flower3.chastota_yxashivania + flower4.chastota_yxashivania) / 4)
-sort_by_price = sorted([flower1.price, flower2.price, flower3.price, flower4.price])
-find_by_lifetime = (
-    flower1.chastota_yxashivania, flower2.chastota_yxashivania, 
-    flower3.chastota_yxashivania, flower4.chastota_yxashivania
-    ) / 4
-print(f'Перкуп цветов говорит, что курс не стабильные, букет стоит {total_price}')
+print(f'В букете {len(my_bouquet.flowers)} цветов')
+print(f'Перкуп цветов говорит, что курс не стабильные, букет стоит: {my_bouquet.total_price_Bouquet()}')
+print(f'Сортировка по увяданию: {my_bouquet.wilting_time()}')
+print(f'Среднее время жизни цветов в букете: {my_bouquet.find_arg_lifetime(3)}')
