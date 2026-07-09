f9ac5b8e1a58d90c7f9503ff147a6090044bb315
Makkarty <ivan_ivanov@mail.com>
2026-07-09 05:19:24 +0300

    Nikita_Kablitsky_homework_13

---
+++ homework/Nick_Kablitsky/Lesson_13/task1.py
@@ -0,0 +1,27 @@
+# Нужно прочитать файлик, который лежит в репозитории в моей папке. Здесь: homework/eugene_okulik/hw_13/data.txt
+# Файлик не копируйте и никуда не переносите. Напишите программу,
+# которая читает этот файл, находит в нём даты и делает с этими датами то, что после них написано.
+# Опирайтесь на то, что структура каждой строки одинакова: сначала идет номер, потом дата,
+# потом дефис и после него текст. У вас должен получиться код, который находит даты и для даты под
+# номером один в коде должно быть реализовано то действие, которое написано в файле после этой даты.
+# Ну и так далее для каждой даты.
+
+data = with open(file: homework/eugene_okulik/hw_13/data.txt,
+    mode: OpenTextMode = "r",
+    buffering: int = -1,
+    encoding: str | None = None,
+    errors: str | None = None,
+    newline: str | None = None,
+    closefd: bool = True,
+    opener: _Opener | None = None)
+
+pattern = (
+    number
+    data
+    defis
+    text
+)
+
+def find_data()
+    
+if data => def number = data_number_of_position
